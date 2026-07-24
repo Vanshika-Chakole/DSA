ee5a80a7067fef2c9cf4d5e870235a25441a0e81
vanshika chakole <vanshikachakole311@gmail.com>
2026-07-24 14:14:57 +0530

    Time: 0 ms (100%), Space: 9 MB (9.73%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-
-        if(n <= 0)
-            return false;
-
-        while(n % 3 == 0) {
-            n = n / 3;
+        if(n > 0 && (int)pow(3,19) % n == 0 ){
+        return true;
         }
-
-        return n == 1;
+        else 
+        {
+           return false;
+        }
     }
 };
