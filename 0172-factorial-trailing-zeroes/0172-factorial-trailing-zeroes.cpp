77bf8d3ba4b4a1d417a85676bfcdf22ff1215285
vanshika chakole <vanshikachakole311@gmail.com>
2026-07-30 14:20:41 +0530

    Time: 0 ms (100%), Space: 8.2 MB (51.17%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+         int x = 0;
+         while(n > 0){
+            n = n / 5;
+            x += n;
+         }
+          return x;
+    }
+   
+}; 
